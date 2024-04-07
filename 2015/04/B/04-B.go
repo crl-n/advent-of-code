179f9f32718e31152c172e29ff403e42cef273f7
Carl Nysten <nysten.carl@gmail.com>
2024-04-07 21:06:04 +0300

    Solve 2015 04-B

---
+++ 2015/04/B/04-B.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"aoc15/utils"
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input := strings.Trim(utils.GetInput(), "\n")
+
+	n := 1
+	var s string
+	var hash [16]byte
+
+	for {
+		s = input + strconv.Itoa(n)
+		hash = md5.Sum([]byte(s))
+		hex := hex.EncodeToString(hash[:])
+		if strings.HasPrefix(hex, "000000") {
+			break
+		}
+		n++
+	}
+
+	fmt.Println(n)
+}
