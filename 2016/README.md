7fbf86a2ee08fab8c1c9e53b430c35fa53b9e8d0
Carl Nysten <nysten.carl@gmail.com>
2024-09-23 22:43:23 +0300

    Add bootstrap.kts script and README.md for 2016

---
+++ 2016/README.md
@@ -0,0 +1,6 @@
+# Advent of Code 2016
+Solved with Kotlin
+
+## Bootstrap Script
+`bootstrap.kts` creates a directory and `.kts` file for solving the exercise.
+It also gets the input from www.adventofcode.com and writes it to a file if not already present.
---
+++ 2016/bootstrap.kts
@@ -0,0 +1,100 @@
+import java.io.File
+import java.net.CookieManager
+import java.net.CookieHandler
+import java.net.HttpCookie
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse.BodyHandlers
+import java.net.URI
+
+fun areValidArgs(args: Array<String>): Boolean {
+    if (args.size != 2) return false
+
+    return isValidDayArg(args[0]) && isValidPartArg(args[1])
+}
+
+fun isValidDayArg(day: String): Boolean {
+    val dayInt = day.toIntOrNull()
+
+    if (dayInt == null) return false
+    if (dayInt >= 0 && dayInt <= 25) return true
+    return false
+}
+
+fun isValidPartArg(part: String): Boolean {
+    val validParts = listOf("A", "B")
+    return validParts.contains(part.toUpperCase())
+}
+
+fun createDayDirectory(day: Int): Boolean {
+    return File("./$day").mkdir()
+}
+
+fun createPartKtsFile(day: Int, part: String): Boolean {
+    return File("./$day/$day-$part.kts").createNewFile()
+}
+
+fun createInputFile(day: Int): Boolean {
+    return File("./$day/$day.input").createNewFile()
+}
+
+fun inputFileExists(day: Int): Boolean {
+    return File("./$day/$day.input").exists()
+}
+
+fun getInput(day: Int): String? {
+    var session = System.getenv("AOCSESSION")
+    if (session == null) {
+        println("Could not get input. Set AOCSESSION env variable to authenticate.")
+        return null
+    }
+
+    println("Requesting input from AOC api")
+
+    var sessionCookie = HttpCookie("session", session)
+    sessionCookie.setPath("/")
+    sessionCookie.setVersion(0)
+
+    var cookieManager = CookieManager()
+    cookieManager.getCookieStore()
+            .add(URI.create("https://adventofcode.com"), sessionCookie)
+
+    var client = HttpClient.newBuilder()
+            .cookieHandler(cookieManager)
+            .version(HttpClient.Version.HTTP_2)
+            .build()
+
+    val request = HttpRequest.newBuilder()
+            .uri(URI.create("https://adventofcode.com/2016/day/$day/input"))
+            .build()
+
+    val response = client.send(request, BodyHandlers.ofString())
+
+    return response.body()
+}
+
+fun bootstrap(day: Int, part: String) {
+    println("Bootstrapping day $day part $part")
+
+    createDayDirectory(day)
+    createPartKtsFile(day, part)
+
+    if (!inputFileExists(day)) {
+        createInputFile(day)
+        val input = getInput(day)
+        input?.let { input -> File("./$day/$day.input").writeText(input) }
+    }
+}
+
+fun main(args: Array<String>) {
+    if (!areValidArgs(args)) {
+        println("Usage: kotlinc -script bootstrap.kts [day] [part]")
+        println("Example: kotlinc -script bootstrap.kts 01 A")
+    } else {
+        val day = args[0].toInt()
+        val part = args[1].toUpperCase()
+        bootstrap(day, part)
+    }
+}
+
+main(args)
