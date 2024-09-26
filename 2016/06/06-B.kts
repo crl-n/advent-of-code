d0b77855e21da907d7c1ae5e6c0414ed0ed6b2d7
Carl Nysten <nysten.carl@gmail.com>
2024-09-26 11:16:53 +0300

    Solve 2016 day 6 part B

---
+++ 2016/06/06-B.kts
@@ -0,0 +1,14 @@
+import java.io.File
+
+val input = File("./06/06.input").readLines()
+
+val transposed = input[0].indices.map { i -> input.map { it[i] }.joinToString("") }
+
+val mostFrequentChars = transposed.map { line ->
+    val charCounts = line.groupingBy { it.code }.eachCount()
+    charCounts.minBy { it.value }.key.toChar()
+}
+
+val solution = mostFrequentChars.joinToString("")
+
+println("Solution: $solution")
