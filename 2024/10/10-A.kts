f202b7c38bd3a458c5ccb3611c9f2edcf2d40306
Carl Nysten <nysten.carl@gmail.com>
2024-12-10 11:04:04 +0200

    Solve 2024 day 10 part A

---
+++ 2024/10/10-A.kts
@@ -0,0 +1,67 @@
+import java.io.File
+
+val input = File("10.input").readLines().map { it.map { char -> char.digitToInt() } }
+val rows = input.size
+val cols = input[0].size
+
+data class Coord(val x: Int, val y: Int) {
+    fun adjacents(): List<Coord> {
+        return listOf(
+            Coord(x, y - 1),
+            Coord(x + 1, y),
+            Coord(x, y + 1),
+            Coord(x - 1, y)
+        ).filter(Coord::isValid)
+    }
+
+    fun value(): Int? {
+        return input.get(y)?.get(x)
+    }
+
+    fun isValid(): Boolean {
+        return y >= 0 && x >= 0 && y < rows && x < cols
+    }
+}
+
+// Find all trailheads
+val trailheads = input.flatMapIndexed { y, row ->
+    row.mapIndexed { x, v -> if (v == 0) Coord(x, y) else null }
+        .filterNotNull()
+}
+
+fun calcScore(trailhead: Coord): Int {
+    // We use a queue of entries consisting of coord and expected value
+    val initialAdjacents = trailhead.adjacents().map { Pair(it, 1) }
+    val queue = ArrayDeque<Pair<Coord, Int>>(initialAdjacents)
+
+    val visited = mutableSetOf<Coord>(trailhead)
+    val trailends = mutableSetOf<Coord>()
+
+    while (queue.isNotEmpty()) {
+        val (coord, expected) = queue.removeFirst()
+
+        if (coord.value() != expected || coord in visited) {
+            continue
+        }
+
+        if (coord.value() == 9) {
+            trailends.add(coord)
+            continue
+        }
+
+        visited.add(coord)
+        val adjacents = coord.adjacents().map { Pair(it, expected + 1) }
+        queue.addAll(adjacents)
+    }
+
+    return trailends.size
+}
+
+var totalScore = 0
+for (th in trailheads) {
+    totalScore += calcScore(th)
+}
+
+println("Solution: $totalScore")
+
+
